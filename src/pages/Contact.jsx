903a03b7f30521ab9b46cd88827d2786729f722a
Md Najeebur Rahman <iamnajeebjung@gmail.com>
2025-09-17 22:26:46 +0530

    Update backend URL

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -28,7 +28,7 @@ export default function Contact() {
             }
         });
         try {
-            const response = await fetch('http://localhost:5000/send-email', {
+            const response = await fetch('https://portfolio-server-jya9.onrender.com/send-email', {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData)
